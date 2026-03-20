33d24b5e23b48defe390dfcdba20c19c32f34434
Fabio Gonçalves <fabiothewarrior@hotmail.com>
2026-03-20 17:02:12 -0300

    feat: Criaçãos das paginas e roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   
   return (
-    <>
+      <Routes>
+          <Route path='/' index element = {<Home />}/>
+      </Routes>
       
-    </>
   )
 }
 
-export default App
+export default App;
