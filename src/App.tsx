9d256cff8b20eacbdd2de50fd96842f9fb926d13
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2026-03-23 22:55:06 -0300

    feat: add Tailwind CSS e estilos iniciais adc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import Home from './pages/Home';
+import Onboarding from './pages/Onboarding';
+import Profile from './pages/Profile';
+import Auth from './pages/Auth';
+import Account from './pages/Account';
 
 function App() {
   
   return (
       <Routes>
           <Route path='/' index element = {<Home />}/>
+          <Route path='/onbording' element = {<Onboarding />}/>
+          <Route path='/profile' element = {<Profile />}/>
+          <Route path='/auth/:pathname' element = {<Auth />}/>
+          <Route path='/account/:pathname' element = {<Account />}/>
       </Routes>
       
   )
